0fb3ba4a4b3ee5716be62803335da07ee703eb4a
stepanbog <step300699@yandex.ru>
2019-11-20 13:04:33 +0300

    Scripted auto-commit on change (2019-11-20 13:04:33) by gitwatch.sh

--- src/main/java/AirportReducer.java
+++ src/main/java/AirportReducer.java
@@ -4,7 +4,8 @@ import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
 import java.util.Iterator;
 
-public class AirportReducer extends Reducer <AirportKey, Text,Text, Text> {
+public class AirportReducer extends Reducer <AirportKey, Text,Text, Text>
+{
     public void reduce(AirportKey key, Iterable<Text> text, Context context) throws IOException, InterruptedException {
         Iterator<Text> it = text.iterator();
         String airportName = it.next().toString();
