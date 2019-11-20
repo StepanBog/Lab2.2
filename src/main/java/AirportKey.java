ea41c28e763d57d9238e2524a6bebfbfef852345
stepanbog <step300699@yandex.ru>
2019-11-20 14:13:13 +0300

    Scripted auto-commit on change (2019-11-20 14:13:13) by gitwatch.sh

--- src/main/java/AirportKey.java
+++ src/main/java/AirportKey.java
@@ -8,36 +8,42 @@ import java.io.IOException;
 
 public class AirportKey implements WritableComparable<AirportKey> {
     private Text Day_Of_Week;
-    private int AirPortID;
+    private IntWritable AirPortID;
 
     public AirportKey() {
         this.Day_Of_Week = new Text();
-        this.AirPortID = 0;
+        this.AirPortID = new IntWritable();
     }
 
     public AirportKey(String value, int num) {
         this.Day_Of_Week = new Text(value);
-        this.AirPortID = num;
+        this.AirPortID = new IntWritable(num);
+    }
+
+    public int compareTo(AirportKey airoportKey) {
+        int res  = Day_Of_Week.compareTo(airoportKey.Day_Of_Week);
+        if (res == 0) {
+            return this.AirPortID.compareTo(airoportKey.AirPortID);
+        }
+        return res;
     }
 
 
-
     public void write(DataOutput dataOutput) throws IOException {
-        airoportName.write(dataOutput);
-        num.write(dataOutput);
+        Day_Of_Week.write(dataOutput);
+        AirPortID.write(dataOutput);
     }
 
 
     public void readFields(DataInput dataInput) throws IOException {
-        airoportName.readFields(dataInput);
-        num.readFields(dataInput);
+        Day_Of_Week.readFields(dataInput);
+        AirPortID.readFields(dataInput);
     }
 
-    public Text getAiroportName() {
-        return airoportName;
+    public Text getDay_Of_Week() {
+        return Day_Of_Week;
     }
-
-    public int compareTo(AirportKey airportKey) {
-        return 0;
+    public IntWritable getAirPortID(){
+        return AirPortID;
     }
 }
