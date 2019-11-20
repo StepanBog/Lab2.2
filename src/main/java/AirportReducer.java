90710a82daa600ffc7f821d3484314685a897474
stepanbog <step300699@yandex.ru>
2019-11-20 14:16:03 +0300

    Scripted auto-commit on change (2019-11-20 14:16:03) by gitwatch.sh

--- src/main/java/AirportReducer.java
+++ src/main/java/AirportReducer.java
@@ -8,25 +8,25 @@ public class AirportReducer extends Reducer <AirportKey, Text,Text, Text>
 {
     public void reduce(AirportKey key, Iterable<Text> text, Context context) throws IOException, InterruptedException {
         Iterator<Text> it = text.iterator();
-        String airportName = it.next().toString();
+        String str = "";
+        String str1 = "";
+        str1 = it.next().toString();
+     /*   String airportName = it.next().toString();
         int minTime = Integer.MAX_VALUE;
         int maxTime = Integer.MIN_VALUE;
         int middleTime = 0;
-        int count = 0;
+        int count = 0;*/
         while (it.hasNext()) {
-            String Time = it.next().toString();
+            str += it.next().toString();
+         /*   String Time = it.next().toString();
             int time = Integer.parseInt(Time);
             maxTime = Math.max(time,maxTime);
             minTime = Math.min(time,minTime);
             count++;
-            middleTime += time;
+            middleTime += time;*/
         }
-
-        if (count > 0 )
-            middleTime /= count;
-        else
-            return;
-        context.write(new Text(airportName), new Text("Average time = " + middleTime + " Max time = " + maxTime + " Min time = " + minTime));
+        context.write(new Text(str1), new Text(str));
+      //  context.write(new Text(airportName), new Text("Average time = " + middleTime + " Max time = " + maxTime + " Min time = " + minTime));
 
     }
 }
